f1f046328f3aad6f5e5775c281667c1dba2501a2
Lucas Godoy <lucasgodoyjor@gmail.com>
2024-12-10 15:40:27 -0300

    Aula 02 finalizada

---
+++ Aula 02 - Controle de fluxo I/Aula 02 - Truthy-falsy.js
@@ -0,0 +1,28 @@
+// Truthy falsy São expressões avaliadas em verdadeiro ou falso
+
+// true
+// Avaliados em verdadeiro:
+// true
+// array vazio
+// número diferente de zero
+// objeto vazio
+// string não vazia, diferente de ''
+
+//falsy
+//Avaliados como falso:
+//null
+//undefined
+//0
+//''
+//NaN
+
+//! operação booleano que indica não
+//!! negar duas vezes, vira ele mesmo em booleano
+
+console.log(!!false) //false
+console.log(!false) //true
+
+console.log (!![], !!{}, !!1, !!'asdaa2')
+
+console.log([] && {} && 'asdas1' && true)
+
