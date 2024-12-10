a061530108299f535e9b165a3326aadda2f17319
Lucas Godoy <lucasgodoyjor@gmail.com>
2024-12-10 19:01:52 -0300

    Aula 03

--- Aula 03 - Controle de fluxo II/Aula 03 - EncontraUsuario.js
+++ Aula 03 - Controle de fluxo II/Aula 03 - EncontraUsuario.js
@@ -2,37 +2,40 @@
 
 const listeDeContatos = ["Paulo", "Selina", "Roger", "Zeca", "Jousé", "Natália", "Bianca"]
 
+// Método 01
+
 let buscaUsuario = true
 let i = 0
 
-// método 01
-// while (buscaUsuario) {
-//     //se encontrar usuário
-//     if (listeDeContatos[i].startsWith('Y')){
-//         buscaUsuario = false
-//         console.log(`Usuário encontrado ${listeDeContatos[i]}`)
-//     }
-//     i += 1 
-
-//     //se não encontrar usuário
-//     if (i === listeDeContatos.length) {
-//         buscaUsuario = false
-//         console.log(`Usuário não encontrado`)
-//     }
-// }
-
-// Método 02
-while (true) {
+while (buscaUsuario) {
     //se encontrar usuário
     if (listeDeContatos[i].startsWith('Y')){
+        buscaUsuario = false
         console.log(`Usuário encontrado ${listeDeContatos[i]}`)
-        break
     }
     i += 1 
 
     //se não encontrar usuário
     if (i === listeDeContatos.length) {
+        buscaUsuario = false
         console.log(`Usuário não encontrado`)
-        break
     }
 }
+
+// Método 02 mais imperativo, menos recomendado
+
+// while (true) {
+//     //se encontrar usuário
+//     if (listeDeContatos[i].startsWith('Y')){
+//         console.log(`Usuário encontrado ${listeDeContatos[i]}`)
+//         break // sai do loop mais interno que há nesse fluxo de execução
+//     }
+//     i += 1 
+
+//     //se não encontrar usuário
+//     if (i === listeDeContatos.length) {
+//         console.log(`Usuário não encontrado`)
+//         break
+//     }
+// }
+
--- Aula 03 - Controle de fluxo II/Ayla 03 - Par ou impar.js
+++ Aula 03 - Controle de fluxo II/Ayla 03 - Par ou impar.js
@@ -1,3 +1,5 @@
+// While pode ser usado até com um índice indeterminado
+
 const listaDeNumeros = [25, 38, 88, 192, 333, 222, 10, 11, 62]
 
 // Estruturas de dados podem ser manipuladas, mesmo sendo const. Pois, não estão sendo reatribuídas.
@@ -7,10 +9,10 @@ const listaDeImpares = []
 let i = 0
 
 while ( i < listaDeNumeros.length) {
-    if (listaDeNumeros[i] % 2 === 0) {
+    if (listaDeNumeros[i] % 2 === 0) { // números divisíveis por 2 tem o resto da divisão igual a zero e são pares
         listaDePares.push(listaDeNumeros[i])
     }
-    else {
+    else { // números cujo resto da divisão por 2 não é zero, são ímpares
         listaDeImpares.push(listaDeNumeros[i])
     }
     i += 1;
@@ -19,3 +21,4 @@ while ( i < listaDeNumeros.length) {
 console.log(`Lista de números: ${listaDeNumeros}`)
 console.log(`Lista de pares: ${listaDePares}`)
 console.log(`Lista de ímpares: ${listaDeImpares}`)
+
