34d2beb40aab5c91299fc9f5bd6b5d4069c66814
Lucas Godoy <lucasgodoyjor@gmail.com>
2024-12-10 09:04:52 -0300

    Adicionado arquivo da Aula 01

---
+++ Aula 01 - Intro.js
@@ -0,0 +1,18 @@
+//Aula 01 - Introdução
+
+let numero = 20 // Declaração variável
+const numeroFavorito = 5 // Declaração constantegf
+
+const estaChovendo = true
+
+const numeroInt = 5.0 // Casa decimal não relevante, número inteiro
+const numeroReal = 5.5 // Casa decimal relevante, número real
+
+const name = 'Lucas'
+
+console.log(numero)
+console.log(numeroFavorito)
+
+console.log(`Oi ${name}! Seu número favorito é: ${numeroFavorito}`) // imprimindo uma variável entre um texto usa-se crases+${nomevariável}
+
+console.log(`Condição do clima é chuvoso: ${estaChovendo}`)
